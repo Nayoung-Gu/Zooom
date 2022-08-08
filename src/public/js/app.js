0048e62d1bf1e681fe26b09d11b9bd788c43abd9
Nayoung-Gu <skdud000322@naver.com>
2022-08-08 21:52:22 +0900

    ✨ Add scripts to save nicknames

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,27 +1,41 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 // In app.js socket means connection to the server
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to the Server ✅");
 });
 socket.addEventListener("message", (message) => {
-    console.log("New Message from the Server: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 socket.addEventListener("close", () => {
     console.log("Disconnected from the Server ❌");
 });
 
-// setTimeout(() => {
-//     socket.send("hello from the browser!");
-// }, 10000);
-
+// Submit to send a message
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+// Sumit to save a nickname
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
     input.value = "";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -21,10 +21,19 @@ const sockets = [];
 // In server.js socket means connected browser
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anonymous";
     console.log("Connected to the Browser ✅");
     // socket.on("close", console.log("Disconnected from the Browser ❌"));
-    socket.on("message", (message) => {
-        sockets.forEach((aSocket) => aSocket.send(message.toString()));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) =>
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                );
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
     });
 });
 
