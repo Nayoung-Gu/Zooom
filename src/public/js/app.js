603ed25106f9a28f76518c6ba0b9543d25764785
Nayoung-Gu <skdud000322@naver.com>
2022-08-08 18:52:09 +0900

    ✨ Connect browser and server using WebSocket

---
+++ src/public/js/app.js
@@ -0,0 +1,16 @@
+// In app.js socket means connection to the server
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to the Server ✅");
+});
+socket.addEventListener("message", (message) => {
+    console.log("New Message from the Server: ", message.data);
+});
+socket.addEventListener("close", () => {
+    console.log("Disconnected from the Server ❌");
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
