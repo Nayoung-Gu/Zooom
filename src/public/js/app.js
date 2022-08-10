3f9b8993e2e4345cd9d8d32d50bc89ea133d476a
Nayoung-Gu <skdud000322@naver.com>
2022-08-10 22:15:41 +0900

    ✨ Add scripts to submit nicknames and messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -9,20 +9,59 @@ room.hidden = true;
 
 let roomName;
 
+function addMessage(msg) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = msg;
+    ul.appendChild(li);
+}
+
+function handleNicknameSubmit(e) {
+    e.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+    input.value = "";
+}
+
+function handleMessageSubmit(e) {
+    e.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value = "";
+}
+
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
 
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
+
+    const nameForm = room.querySelector("#name");
+    const msgForm = room.querySelector("#msg");
+    nameForm.addEventListener("submit", handleNicknameSubmit);
+    msgForm.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(e) {
     e.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", { payload: input.value }, showRoom);
+    socket.emit("enter_room", input.value, showRoom);
     roomName = input.value;
     input.value = "";
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user) => {
+    addMessage(`${user} joined!`);
+});
+
+socket.on("bye", (user) => {
+    addMessage(`${user} left!`);
+});
+
+socket.on("new_message", addMessage);
